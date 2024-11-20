0910ea1b3b9dc72b21210264691c641afbc972ad
Steve Rich <steven.john.rich@gmail.com>
2024-11-20 16:36:12 +0000

    Added primer2 possible solution and added jest helper text at the top of the test file.

--- primer2/primer2.js
+++ primer2/primer2.js
@@ -1,15 +1,44 @@
-// Use these if you wish, delete if you don't
-const cardSuits = new Set(["Hearts", "Diamonds", "Clubs", "Spades"]);
-const cardRanks = new Set(["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]);
-
-// Making sure you are in the same folder, you can run this test file with: jest primer2.test.js
-// To run some tests: jest -t "Shuffle" primer2.test.js (runs all tests with "Shuffle" in their name)
-// To run one specific test, e.g. jest -t "each card should be a valid card string format" primer2.test.js
-function shuffleAndDeal(){
-
-}
-
-
+function shuffleAndDeal(numPlayers, cardsPerPlayer, numDecks = 1) {
+    // Input validation
+    if (numPlayers <= 0 || !Number.isInteger(numPlayers)) {
+      throw new Error("Invalid number of players. Must be a positive integer.");
+    }
+    if (cardsPerPlayer <= 0 || !Number.isInteger(cardsPerPlayer)) {
+      throw new Error("Invalid cards per hand. Must be a positive integer.");
+    }
+    const totalCards = numPlayers * cardsPerPlayer;
+    const deckSize = numDecks * 52;
+    if (totalCards > deckSize) {
+      throw new Error(`Not enough cards in ${numDecks} deck(s) to deal ${cardsPerPlayer} cards to ${numPlayers} players.`);
+    }
+  
+    // Create a deck of cards
+    const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+    const ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    let deck = [];
+  
+    for (let i = 0; i < numDecks; i++) {
+      for (const suit of suits) {
+        for (const rank of ranks) {
+          deck.push(`${rank} of ${suit}`);
+        }
+      }
+    }
+  
+    // Shuffle the deck using Fisher-Yates algorithm (O(n) time complexity)
+    for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+  
+    // Deal cards to players
+    const hands = [];
+    for (let i = 0; i < numPlayers; i++) {
+      hands.push(deck.splice(0, cardsPerPlayer));
+    }
+  
+    return hands;
+  }
 
 export default shuffleAndDeal;
 
