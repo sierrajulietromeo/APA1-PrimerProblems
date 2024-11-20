372522cb9ef4809b41617e760af633ac516cb8c6
Steve Rich <steven.john.rich@gmail.com>
2024-11-20 16:31:06 +0000

    primer2 - updated blank file with TODO list

--- primer2/primer2.js
+++ primer2/primer2.js
@@ -1,33 +1,30 @@
-// Use these if you wish, delete if you don't
-const cardSuits = new Set(["Hearts", "Diamonds", "Clubs", "Spades"]);
-const cardRanks = new Set(["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]);
-
 // Making sure you are in the same folder, you can run this test file with: jest primer2.test.js
 // To run some tests: jest -t "Shuffle" primer2.test.js (runs all tests with "Shuffle" in their name)
 // To run one specific test, e.g. jest -t "each card should be a valid card string format" primer2.test.js
-function shuffleAndDeal(){
 
-}
 
+// TODO: Write a function shuffleAndDeal(numPlayers, cardsPerPlayer, numDecks = 1)
+// that simulates shuffling and dealing a deck of cards.
+
+// TODO: Create a standard 52-card deck (or 104 if numDecks is 2).
+
+// TODO: Implement input validation to handle invalid inputs:
+//       -  numPlayers should be a positive integer.
+//       -  cardsPerPlayer should be a positive integer.
+//       -  Throw an error if the requested cards exceed the deck size.
+
+// TODO: Shuffle the deck using a suitable algorithm
+//       -  Consider time complexity and randomness.
+
+// TODO: Deal cards to the specified number of players.
+
+// TODO: Return the hands dealt as an array of arrays.
+
+// TODO: Test the function with various inputs, including edge cases:
+//       -  Dealing the entire deck.
+//       -  Single player.
+//       -  Minimum cards per hand.
 
 
 export default shuffleAndDeal;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Don't forget to export functions/classes/objects so they are visible to your tests.
